293eae450b7a300b00090af270eed7cedd5a701d
Jeff Allen <1063272+jeffallen6767@users.noreply.github.com>
2018-02-20 20:09:49 -0800

    allow config

--- testing.js
+++ testing.js
@@ -1,4 +1,9 @@
-var colors = require("colors/safe"),
+var 
+  colors = require("colors/safe"),
+  config = {
+    "pass.color": "green",
+    "fail.color": "red"
+  },
   testing = {
     "api": {
       "startTime": function() {
@@ -29,6 +34,14 @@ var colors = require("colors/safe"),
       }
     },
     "data": {},
+    "config": function(conf) {
+      if (typeof conf === "object") {
+        Object.keys(conf).forEach(function(key) {
+          config[key] = conf[key];
+        });
+      }
+      return testing;
+    },
     "init": function(tests) {
       var keys = Object.keys(tests);
       testing.data = {
@@ -54,26 +67,28 @@ var colors = require("colors/safe"),
         data = testing.data,
         idx = data.idx,
         key = data.keys[idx],
-        result = data.results[key];
+        result = data.results[key],
+        formatPassColor = colors[config["pass.color"]];
       result.pass.push(args);
       data.pass++;
       args.unshift(
         testing.timing(result.times)
       );
-      console.log(colors.green(["   PASS!"].concat(args).join(" ")));
+      console.log(formatPassColor(["   PASS!"].concat(args).join(" ")));
     },
     "fail": function() {
       var args = [].slice.call(arguments),
         data = testing.data,
         idx = data.idx,
         key = data.keys[idx],
-        result = data.results[key];
+        result = data.results[key],
+        formatFailColor = colors[config["fail.color"]];
       result.fail.push(args);
       data.fail++;
       args.unshift(
         testing.timing(result.times)
       );
-      console.log(colors.red(["   FAIL!"].concat(args).join(" ")));
+      console.log(formatFailColor(["   FAIL!"].concat(args).join(" ")));
     },
     "results": function() {
       var data = testing.data,
@@ -82,11 +97,13 @@ var colors = require("colors/safe"),
         start = data.start,
         end = data.end,
         mills = end - start,
-        total = pass + fail;
+        total = pass + fail,
+        formatPassColor = colors[config["pass.color"]],
+        formatFailColor = colors[config["fail.color"]];
       console.log(colors.white.bold("results:"));
       console.log(colors.yellow.bold("   ", total, "tests"));
-      console.log(colors.green.bold("   ", pass, "pass"));
-      console.log(colors.red.bold("   ", fail, "fail"));
+      console.log(formatPassColor.bold("   ", pass, "pass"));
+      console.log(formatFailColor.bold("   ", fail, "fail"));
       console.log(colors.white.bold("    Time:", mills, "Milliseconds"));
     },
     "run": function(tests, context) {
